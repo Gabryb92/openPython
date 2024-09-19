2c81f6e0ab074111a6a66e7e18efd88b665a3873
Gabriele <gabriele.bonazza92@gmail.com>
2024-09-19 14:35:53 +0200

    risolto problema css, js non caricati, aggiunto recupero dati per targets da db

--- myapp/services/targets_service.py
+++ myapp/services/targets_service.py
@@ -1,9 +1,12 @@
-from myapp.authentication import GVMBackend
+#from myapp.authentication import GVMBackend
 from django.db import connection
+import datetime
 
 #Targets
 def get_data_targets():
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM targets")  # Modifica 'tua_tabella' con il nome corretto
-        data = cursor.fetchall()
-    return data
+        targets = cursor.fetchall()
+        targets_dict = [{'name': row[3],'host': row[4],'portLists':row[9],'create':datetime.datetime.fromtimestamp(row[12])} for row in targets]
+    return targets_dict
+
