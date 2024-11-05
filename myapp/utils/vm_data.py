d61c59574c08467fc59f2980ee0bedbef5534866
Gabriele Bonazza <gabriele.bonazza92@gmail.com>
2024-11-05 16:03:42 +0100

    aggiunto file .env.example e aggiornato requirements.txt

--- myapp/utils/vm_data.py
+++ myapp/utils/vm_data.py
@@ -1,4 +1,16 @@
+'''
 kali_ip='192.168.99.116'
 kali_username = "openvas"
 kali_password = "openvas"
 private_key_path = '/home/gabrieledev/.ssh/id_rsa'
+'''
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+kali_ip = os.getenv('KALI_IP')
+kali_username = os.getenv('KALI_USERNAME')
+kali_password = os.getenv('KALI_PASSWORD')
+private_key_path = os.getenv('PRIVATE_KEY_PATH')
